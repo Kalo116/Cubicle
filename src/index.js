378e92ae5b06c504246ef99ec686ab8b46c1f5a2
Kalo116 <kaloyan0102@gmail.com>
2022-06-13 15:32:11 +0300

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 
 const app = express();
 
 app.use('/static', express.static('public'));
 
+app.engine('hbs', hbs.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.render('index')
 });
 
 app.listen(5000, () => console.log('App is listening!'));
